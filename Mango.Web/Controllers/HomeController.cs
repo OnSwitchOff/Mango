be0889c5665a1444b643626be9bbe34af4e8f35d
Viktor <kassov@ukr.net>
2023-08-11 10:18:58 +0300

    HomeController

--- Mango.Web/Controllers/HomeController.cs
+++ Mango.Web/Controllers/HomeController.cs
@@ -1,7 +1,9 @@
 using Mango.Web.Models;
+using Mango.Web.Service.IService;
 using Mango.Web.Utility;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 using System.Diagnostics;
 
 namespace Mango.Web.Controllers
@@ -9,15 +11,45 @@ namespace Mango.Web.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+		private readonly IProductService _productService;
 
-        public HomeController(ILogger<HomeController> logger)
-        {
-            _logger = logger;
+		public HomeController(ILogger<HomeController> logger, IProductService productService)
+		{
+			_logger = logger;
+			_productService = productService;
+		}
+
+		public async Task<IActionResult> Index()
+		{
+			List<ProductDto>? list = new();
+			ResponseDto? response = await _productService.GetAllProductsAsync();
+
+			if (response != null && response.IsSuccess)
+			{
+				list = JsonConvert.DeserializeObject<List<ProductDto>>(Convert.ToString(response.Result)!);
+			}
+			else
+			{
+				TempData["error"] = response?.Message;
+			}
+			return View(list);
         }
 
-        public IActionResult Index()
+        [Authorize]
+        public async Task<IActionResult> ProductDetails(int productId)
         {
-            return View();
+            ProductDto obj = new();
+            ResponseDto? response = await _productService.GetProductByIdAsync(productId);
+
+            if (response != null && response.IsSuccess)
+            {
+                obj = JsonConvert.DeserializeObject<ProductDto>(Convert.ToString(response.Result)!);
+            }
+            else
+            {
+                TempData["error"] = response?.Message;
+            }
+            return View(obj);
         }
 
         public IActionResult Privacy()
